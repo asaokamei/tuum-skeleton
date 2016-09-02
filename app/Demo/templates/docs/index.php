be9bc4d7a8322cc4fefabcc11e8dfb71c5d0b0bf
Asao <kamei@workspot.jp>
2016-09-02 22:00:35 +0900

    changed sample app contents.

--- app/Demo/templates/docs/index.php
+++ app/Demo/templates/docs/index.php
@@ -16,8 +16,49 @@ $this->layout('layouts/layout', [
 
 <?php $this->start('contents'); ?>
 
-<h1>Sample Documents</h1>
+<style type="text/css">
+    div.main {
+        text-align: left;
+        line-height: 1.5em;
+        width: 40em;
+        margin: auto;
+        
+    }
+</style>
+<h1>About Slim3 + Tuum</h1>
 
+<div class="main">
+
+    1. A Micro-Framework:
+    
+    <ul>
+        <li><a href="http://www.slimframework.com/" >Slim 3</a>: 
+            A micro-framework for PHP. </li>
+
+    </ul>
+    
+    2. Add a Respond Module for creating views (and redirect and errors):
+    
+    <ul>
+        <li><a href="https://github.com/TuumPHP/Respond" >Tuum/Respond</a>: 
+            A view module for various micro-frameworks based on PSR-7. </li>
+        
+        <li><a href="https://github.com/TuumPHP/Builder" >Tuum/Builder</a>:
+            A generic application builder to manage environment etc. </li>
+
+        <li><a href="https://github.com/TuumPHP/FileMap" >Tuum/FileMap</a>:
+            file mapper for providing response. </li>
+    </ul>
+
+    3. Some Other Popular Projects:
+    
+    <ul>
+        <li>Monolog: a de fact standard logger library. </li>
+        <li>League/Plates: raw PHP as a template. </li>
+    </ul>
+</div>
+
+<p>&nbsp;</p>
 <p>This is a PHP document rendered by FileMap. </p>
 
 <?php $this->stop(); ?>
--- app/Demo/templates/layouts/layout.php
+++ app/Demo/templates/layouts/layout.php
@@ -11,7 +11,6 @@ use Tuum\Respond\Service\ViewHelper;
 <head>
     <meta charset="utf-8"/>
     <title><?= isset($title) ? $title . ' / ': ''; ?>Tuum + Slim 3</title>
-    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
     <style>
         body {
             margin: 50px 0 0 0;
@@ -19,23 +18,34 @@ use Tuum\Respond\Service\ViewHelper;
             width: 100%;
             font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
             text-align: center;
-            color: #aaa;
+            color: #40664c;
             font-size: 18px;
         }
 
-        h1#slim {
-            color: #719e40;
-            letter-spacing: -3px;
-            font-family: 'Lato', sans-serif;
+        h1#title {
+            color: #586a87;
+            letter-spacing: -1px;
+            font-family: "Helvetica Neue", 'Lato', sans-serif;
             font-size: 100px;
             font-weight: 200;
             margin-bottom: 0;
+            margin-left: -.2em;
+        }
+        span.sub-title {
+            font-size:16px;
+            letter-spacing: normal;
+        }
+        div.header {
+            margin-left:4em;
+            text-align: left;
         }
     </style>
 </head>
 <body>
-<h1 id="slim">Slim+Tuum</h1>
-<div><a href="/" >a micro-framework for PHP</a></div>
+<div class="header">
+    <h1 id="title">Slim 3 + Tuum</h1>
+    <a href="/" ><span class="sub-title">a micro-framework with a view.</span></a>
+</div>
 
 <br/>
 <hr color="#e0e0e0" size="1">
